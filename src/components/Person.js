f9182ee73b28777964afb1d7ca9b65f07057d326
Satinder Singh <agnosticoder@gmail.com>
2021-01-07 13:06:09 -0500

    React cause effect without react query

---
+++ src/components/Person.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+const Person = ({street, city, state, country, telephone, birthday}) => {
+
+
+
+    
+    return (
+        <div>
+            <div> 
+                <div>Street: {street}</div>
+                <div>City: {city}</div>
+                <div>State: {state}</div>
+                <div>Country: {country}</div>
+                <div>Telephone: {telephone}</div>
+                <div>Birthday: {birthday}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Person;
---
+++ src/data/people.js
@@ -0,0 +1,65 @@
+
+import faker from 'faker';
+
+
+const people = [
+    {
+        name: faker.name.findName(),
+        street: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+        telephone: faker.phone.phoneNumber(),
+        birthday: faker.date.past().toDateString()
+    },
+    {
+        name: faker.name.findName(),
+        street: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+        telephone: faker.phone.phoneNumber(),
+        birthday: faker.date.past().toDateString()
+    },
+    {
+        name: faker.name.findName(),
+        street: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+        telephone: faker.phone.phoneNumber(),
+        birthday: faker.date.past().toDateString()
+    },
+    {
+        name: faker.name.findName(),
+        street: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+        telephone: faker.phone.phoneNumber(),
+        birthday: faker.date.past().toDateString()
+    },
+    {
+        name: faker.name.findName(),
+        street: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+        telephone: faker.phone.phoneNumber(),
+        birthday: faker.date.past().toDateString()
+    }
+]
+
+export const getpeople = (humans) => {
+    return new Promise((resolve, reject) => {
+      if(humans){
+          setTimeout(() => {
+            resolve(people);
+          }, 2000)
+      }else{
+          setTimeout(() => reject(new Error("Something went wrong")), 2000);
+      }
+    })
+}
+
+export default people;
